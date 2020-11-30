bca93c940710d188f2b2732728c04e1cbff1c8fb
Benjamin Pavie <u0094799@kuleuven.be>
2020-11-30 22:51:10 +0100

    Fixed ij_proeminence name in wrapper.py

--- wrapper.py
+++ wrapper.py
@@ -22,7 +22,7 @@ def main(argv):
         # 3. Call the image analysis workflow using the run script
         nj.job.update(progress=25, statusComment="Launching workflow...")
         command = "/usr/bin/xvfb-run java -Xmx6000m -cp /fiji/jars/ij.jar ij.ImageJ --headless --console " \
-                  "-macro macro.ijm \"input={}, output={}, radius={}, proeminence={}\"".format(in_path, out_path, nj.parameters.ij_radius, nj.parameters.ij_proeminenced)
+                  "-macro macro.ijm \"input={}, output={}, radius={}, proeminence={}\"".format(in_path, out_path, nj.parameters.ij_radius, nj.parameters.ij_proeminence)
         return_code = call(command, shell=True, cwd="/fiji")  # waits for the subprocess to return
 
         if return_code != 0:
